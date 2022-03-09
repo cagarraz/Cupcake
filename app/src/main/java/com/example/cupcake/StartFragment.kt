9101441b87329a34b1bbb3ca05347564f4aa44ed
Carlos <cagarraz@unirioja.es>
2022-03-09 18:21:23 +0100

    tercer commit

--- app/src/main/java/com/example/cupcake/StartFragment.kt
+++ app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -1,20 +1,6 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.cupcake
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -23,19 +9,14 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentStartBinding
+import com.example.cupcake.databinding.FragmentSummaryBinding
 import com.example.cupcake.model.OrderViewModel
 
-/**
- * This is the first screen of the Cupcake app. The user can choose how many cupcakes to order.
- */
+
 class StartFragment : Fragment() {
 
-    // Binding object instance corresponding to the fragment_start.xml layout
-    // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
-    // when the view hierarchy is attached to the fragment.
     private var binding: FragmentStartBinding? = null
 
-    // Use the 'by activityViewModels()' Kotlin property delegate from the fragment-ktx artifact
     private val sharedViewModel: OrderViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -52,9 +33,6 @@ class StartFragment : Fragment() {
         binding?.startFragment = this
     }
 
-    /**
-     * Start an order with the desired quantity of cupcakes and navigate to the next screen.
-     */
     fun orderCupcake(quantity: Int) {
         // Update the view model with the quantity
         sharedViewModel.setQuantity(quantity)
@@ -68,10 +46,6 @@ class StartFragment : Fragment() {
         findNavController().navigate(R.id.action_startFragment_to_flavorFragment)
     }
 
-    /**
-     * This fragment lifecycle method is called when the view hierarchy associated with the fragment
-     * is being removed. As a result, clear out the binding object.
-     */
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
